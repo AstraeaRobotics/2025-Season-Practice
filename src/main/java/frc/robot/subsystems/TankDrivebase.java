17fefd45401e9f2c444adae95aafb6c30ad3b211
Sobhard <sohumb999@gmail.com>
2025-01-12 19:04:59 -0500

    added smartdashboard for tankdrivebase

--- src/main/java/frc/robot/subsystems/TankDrivebase.java
+++ src/main/java/frc/robot/subsystems/TankDrivebase.java
@@ -9,6 +9,7 @@ import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive.WheelSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -53,6 +54,7 @@ public class TankDrivebase extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("TankDrive Encoder", getEncoder());
     // This method will be called once per scheduler run
   }
 }
